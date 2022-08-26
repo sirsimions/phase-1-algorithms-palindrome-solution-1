6027a46eb0e5aa2a3798f51451154dced098daf9
Simion <simion.okola@student.moringaschool.com>
2022-08-26 03:27:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
+  function isPalindrome(word) {
+    // reverse the input string
+    const reversedWord = reverseString(word);
+    // if the reversed string is the same as the input
+    if (word === reversedWord) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
